ffb674fcde65655c86ba8f5096a87491d64d560d
Alejandro Piad <alepiad@gmail.com>
2016-07-19 16:28:32 -1000

    Update https

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
 
         //Change this to your own rss feeds
-        "http://blog.apiad.net/feed.xml",
+        "https://blog.apiad.net/feed.xml",
 
         {
         // how many entries do you want?
